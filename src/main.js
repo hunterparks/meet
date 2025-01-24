15bf8e1e3e7f5a2a385f1a56e9fef8de910a6e89
Hunter Parks <hunter@prks.cc>
2025-01-24 00:55:54 -0600

    :sparkles: Add side panel

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import { meet } from '@googleworkspace/meet-addons';
 const CLOUD_PROJECT_NUMBER = '530312883416';
 const MAIN_STAGE_URL = 'https://hunterparks.github.io/meet/standupper-meet/MainStage.html';
 
+export async function setUpAddon() {
+    const session = await meet.addon.createAddonSession({
+        cloudProjectNumber: CLOUD_PROJECT_NUMBER
+    });
+    const sidePanelClient = await session.createSidePanelClient();
+    document.getElementById('start-activity')
+        .addEventListener('click', async () => {
+            await sidePanelClient.startActivity({ mainStageUrl: MAIN_STAGE_URL });
+        })
+}
+
 export async function initializeMainStage() {
     const session = await meet.addon.createAddonSession({
         cloudProjectNumber: CLOUD_PROJECT_NUMBER
